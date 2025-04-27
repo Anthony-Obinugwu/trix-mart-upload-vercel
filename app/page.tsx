b855b90cfd0c9f2c32e31c71e09eb88c03d9f1a7
toxicdev <obinugwuchinedu1@gmail.com>
2025-04-27 13:43:50 +0100

    uplaod fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 'use client';
+
+
 import {ChangeEvent, FormEvent, useState, useRef, useEffect} from "react";
 import Image from "next/image";
-import Head from "next/head";
 import {useRouter, useSearchParams} from 'next/navigation';
 
 type UploadMessage = {
@@ -156,10 +157,7 @@ export default function Home() {
 
   return (
       <>
-        <Head>
-          <title>Upload Student ID - TrixMart</title>
-          <meta name="description" content="Upload your student ID for verification" />
-        </Head>
+
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
             <div
